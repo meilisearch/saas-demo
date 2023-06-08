4c6fad9bba58083830c7362bba30339d19b2072b
CaroFG <carolina.ferreira131@gmail.com>
2023-06-08 17:14:01 +0200

    Add atoms/Card component

---
+++ resources/views/stories/atoms/card.blade.php
@@ -0,0 +1,17 @@
+@storybook([
+    'name' => 'Card',
+    'args' => [
+        'cardTitle' => 'Title',
+        'cardBody' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos dicta ad at nesciunt corrupti!',
+        'cardFooter' => 'Footer'
+    ],
+    'argTypes' => [
+    ]
+])
+
+<x-atoms.card
+    :$cardTitle
+    :$cardBody
+    :$cardFooter
+>
+</x-atoms.card>

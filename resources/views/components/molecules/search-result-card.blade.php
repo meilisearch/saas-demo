ba33f04e6d41307a5e37343863f16d56e4dc8269
CaroFG <carolina.ferreira131@gmail.com>
2023-06-13 10:15:40 +0200

    Create molecules/search-result-card component

---
+++ resources/views/components/molecules/search-result-card.blade.php
@@ -0,0 +1,16 @@
+@props(['title'])
+
+<x-atoms.card>
+    <div class="sm:flex sm:justify-start sm:gap-4 items-center">
+            {{ $picture }}
+        <h3 class="text-base font-bold text-gray-900 sm:text-xl">
+            {{ $title }}
+        </h3>
+    </div>
+
+    <div class="mt-4">
+        <p class="max-w-[40ch] text-sm text-gray-500">
+            {{ $slot }}
+        </p>
+    </div>
+</x-atoms.card>

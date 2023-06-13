77b0cb698c929f1ff2c33a2b8bdab5dfd966bf54
CaroFG <carolina.ferreira131@gmail.com>
2023-06-13 11:18:41 +0200

    Transform title property into slot in search result card

--- resources/views/stories/molecules/search-result-card.blade.php
+++ resources/views/stories/molecules/search-result-card.blade.php
@@ -1,18 +1,20 @@
 @storybook([
     'name' => 'Search result card',
     'args' => [
-        'title' => 'Title',
-        'pictureUrl' => Vite::asset('resources/assets/meilisearch-logo.svg')
+        'pictureUrl' => Vite::asset('resources/assets/meilisearch-logo.svg'),
+        'organizationName' => 'Meilisearch'
     ],
     'argTypes' => [
-        'title' => 'text',
         'pictureUrl' => 'text'
     ],
 ])
 
-<x-molecules.search-result-card :$title>
+<x-molecules.search-result-card>
+    <x-slot:title>
+        Title
+    </x-slot>
     <x-slot:picture>
-        <x-atoms.organization-logo :logo-url="Vite::asset('resources/assets/meilisearch-logo.svg')" :organization-name="$title"></x-atoms.organization-logo>
+        <x-atoms.organization-logo :logo-url="Vite::asset('resources/assets/meilisearch-logo.svg')" :organization-name="$organizationName"></x-atoms.organization-logo>
     </x-slot>
     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quis nihil nemo maxime tempora vel. Aperiam, illo. Nihil, facilis.</p>
 </x-molecules.search-result-card>

37a0bf7feec639a19fd6f0033f8adff5b6b9589e
Strift <laurent@meilisearch.com>
2023-06-14 16:25:11 +0200

    Add organizations

---
+++ database/seeders/OrganizationSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Organization;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrganizationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Organization::factory()->count(2)->create();
+    }
+}
--- tests/Feature/DatabaseSeederTest.php
+++ tests/Feature/DatabaseSeederTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Organization;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,12 +11,25 @@ class DatabaseSeederTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    /**
+     * @test
+     */
+    public function itSeedsTwoOrganizations(): void
+    {
+        $this->assertCount(2, Organization::all());
+    }
+
     /**
      * @test
      */
     public function itSeedsTwoUsersFromConfiguration(): void
     {
-        $this->seedUsers();
         $this->assertDatabaseHas('users', [
             'email' => config('app.seeders.user1.email'),
             'avatar_url' => 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',

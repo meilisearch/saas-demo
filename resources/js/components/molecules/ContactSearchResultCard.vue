69effa3dd682c0c167ec0e95db6144a118f1d210
CaroFG <carolina.ferreira131@gmail.com>
2023-06-26 14:17:53 +0200

    Add search results cards for deal, contact and company

---
+++ resources/js/components/molecules/ContactSearchResultCard.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+    import SearchResultCard from './SearchResultCard.vue';
+    import Avatar from '../atoms/Avatar.vue';
+    import MailIcon from '../atoms/MailIcon.vue'
+    import PhoneIcon from '../atoms/PhoneIcon.vue'
+
+    export interface Props {
+    href?: string,
+    contactAvatarUrl: string
+    }
+
+    const props = withDefaults(defineProps<Props>(), {
+    href: '#'
+    })
+</script>
+
+<template>
+    <SearchResultCard :href="props.href">
+        <template #picture>
+            <Avatar :image-url="props.contactAvatarUrl"/>
+        </template>
+        <template #title>
+            <slot name="contactFullName"></slot>
+        </template>
+        <template #subtitle>
+            <slot name="contactCompanyName"></slot>
+        </template>
+        <template #default>
+            <div class="flex items-center">
+                <MailIcon class="mr-2" />
+                <slot name="contactEmail"></slot>
+            </div>
+            <div class="mt-2 flex items-center">
+                <PhoneIcon class="mr-2"/>
+                <slot name="contactPhoneNumber"></slot>
+            </div>
+        </template>
+    </SearchResultCard>
+</template>
---
+++ resources/js/components/molecules/DealSearchResultCard.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+    import SearchResultCard from './SearchResultCard.vue';
+
+    export interface Props {
+    href?: string
+    }
+
+    const props = withDefaults(defineProps<Props>(), {
+    href: '#'
+    })
+</script>
+
+<template>
+    <SearchResultCard :href="props.href">
+        <template #picture>
+            <slot name="dealStatusIcon"></slot>
+        </template>
+        <template #title>
+            <slot name="companyName"></slot>
+        </template>
+        <template #subtitle>
+            <slot name="dealValue"></slot>
+        </template>
+        <template #default>
+            <slot name="dealInfo"></slot>
+        </template>
+    </SearchResultCard>
+</template>

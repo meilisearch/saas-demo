717c38b0cd8f84063df0806a33ea6a0cee655aaf
CaroFG <carolina.ferreira131@gmail.com>
2023-06-26 14:13:49 +0200

    Add avatar and org logo as vue components

---
+++ resources/js/components/atoms/Avatar.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+    const props = defineProps<{
+    imageUrl: string,
+    }>()
+</script>
+
+<template>
+    <div class="h-10 w-10 overflow-hidden rounded-full">
+        <img :src="props.imageUrl" class="object-cover" />
+    </div>
+</template>
---
+++ resources/js/components/atoms/OrganizationLogo.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+    const props = defineProps<{
+    organizationName: string,
+    logoUrl: string
+    }>()
+</script>
+
+
+<template>
+    <div class="flex h-10 w-10 items-center overflow-hidden rounded bg-gray-100 p-1">
+        <span class="sr-only">Logo</span>
+        <img :src="props.logoUrl" :alt="props.organizationName">
+    </div>
+</template>

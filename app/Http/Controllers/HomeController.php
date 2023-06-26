a42fd8d5be73af69180052afbe597a4d77acb3d5
Strift <laurent@meilisearch.com>
2023-06-26 18:25:33 +0200

    Remove home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,20 +14,6 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = null;
-        if (Auth::check()) {
-            $user = Auth::user();
-        } else {
-            $user = User::findById(1);
-            Auth::login($user);
-        }
-        return view('home')->with([
-            'userName' => $user->name,
-            'userAvatarUrl' => $user->avatar_url,
-            'userEmail' => $user->email,
-            'otherUserEmail' => User::where('email', '!=', $user->email)->first()->email,
-            'organizationLogoUrl' => Vite::asset('resources/assets/meilisearch-logo.svg'),
-            'organizationName' => $user->organization->name,
-        ]);
+        return redirect()->action([DealController::class, 'index']);
     }
 }

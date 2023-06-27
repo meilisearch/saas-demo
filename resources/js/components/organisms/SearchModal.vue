498dafedb81dcd04f56d9dadb35361303cfc3039
CaroFG <carolina.ferreira131@gmail.com>
2023-06-27 09:20:43 +0200

    Remove extra spaces and useless ref to slot

--- resources/js/components/organisms/SearchModal.vue
+++ resources/js/components/organisms/SearchModal.vue
@@ -1,4 +1,4 @@
-<template #default>
+<template>
     <div class="z-100 fixed top-0 right-0 flex h-full w-full justify-center overflow-hidden bg-gray-200/50 p-4 pt-6">
         <div class="flex max-h-[70vh] w-[52rem] flex-col rounded-2xl border border-blue-100 bg-white p-4 shadow-lg sm:p-6 lg:p-8 divide-y divide-violet-100 mt-36">
             <header class="w-full mb-4">
@@ -10,7 +10,3 @@
         </div>
     </div>
 </template>
-
-
-
-

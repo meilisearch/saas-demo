59c881271769d0e08f8501cb01b5b51f5dd76e50
Strift <laurent@meilisearch.com>
2023-06-28 10:03:22 +0200

    Move JS components to components/organisms

--- resources/js/table-search.js
+++ resources/js/table-search.js
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import InstantSearch from 'vue-instantsearch/vue3/es'
 
 // Components
-import Meilisearch from './components/Meilisearch.vue'
-import SearchBar from './components/SearchBar.vue'
-import SearchFilterMenu from './components/SearchFilterMenu.vue'
-import SearchSortBy from './components/SearchSortBy.vue'
-import DealList from './components/DealList.vue'
-import ContactList from './components/ContactList.vue'
-import CompanyList from './components/CompanyList.vue'
+import Meilisearch from './components/organisms/Meilisearch.vue'
+import SearchBar from './components/organisms/SearchBar.vue'
+import SearchFilterMenu from './components/organisms/SearchFilterMenu.vue'
+import SearchSortBy from './components/organisms/SearchSortBy.vue'
+import DealList from './components/organisms/DealList.vue'
+import ContactList from './components/organisms/ContactList.vue'
+import CompanyList from './components/organisms/CompanyList.vue'
 
 const app = createApp({
   components: {

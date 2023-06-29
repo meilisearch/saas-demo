0fad880dcbd8ea5dd4d9a7224f41daf653944902
CaroFG <carolina.ferreira131@gmail.com>
2023-06-29 16:14:36 +0200

    Add fixed height to modal

--- resources/js/components/organisms/ModalBox.vue
+++ resources/js/components/organisms/ModalBox.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex max-h-[70vh] w-[52rem] flex-col rounded-2xl border border-blue-100 bg-white p-4 shadow-lg sm:p-6 lg:p-8 divide-y divide-violet-100">
+    <div class="flex h-[70vh] w-[52rem] flex-col rounded-2xl border border-blue-100 bg-white p-4 shadow-lg sm:p-6 lg:p-8 divide-y divide-violet-100">
         <header class="w-full mb-4">
             <slot name="searchbar"></slot>
         </header>

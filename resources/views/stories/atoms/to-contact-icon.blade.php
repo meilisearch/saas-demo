96cbf5c63e6f78da9772035ece9b17767b5a8112
CaroFG <carolina.ferreira131@gmail.com>
2023-06-30 15:17:18 +0200

    Remove unused icons

--- resources/views/stories/atoms/to-contact-icon.blade.php
+++
@@ -1,8 +0,0 @@
-@storybook([
-    'name' => 'To Contact Icon',
-    'args' => [],
-    'argTypes' => [],
-])
-
-<x-atoms.to-contact-icon>
-</x-atoms.to-contact-icon>
--- resources/views/stories/atoms/won-icon.blade.php
+++
@@ -1,8 +0,0 @@
-@storybook([
-    'name' => 'Won Icon',
-    'args' => [],
-    'argTypes' => [],
-])
-
-<x-atoms.won-icon>
-</x-atoms.won-icon>

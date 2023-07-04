292a4b0d0cc4a270027ea0b8a12046d5fdbd5316
Strift <laurent@meilisearch.com>
2023-07-04 14:54:11 +0200

    Update Vite config for built files

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/js/search-modal',
+                'resources/js/table-search'
+            ],
             refresh: true,
         }),
         vue({

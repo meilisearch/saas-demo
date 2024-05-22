3b95d536d057a9b8de06f982b8a41f304ba9fb27
Strift <laurent@meilisearch.com>
2024-05-22 12:16:28 +0200

    rename component

--- resources/js/components/organisms/CompanySearchResults.vue
+++ resources/js/components/organisms/CompanySearchResults.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { AisIndex, AisInfiniteHits, AisHighlight } from 'vue-instantsearch/vue3/es'
-import CompanySearchResultCard from '../molecules/CompanySearchResultCard.vue';
+import CompanySearchResultsItem from '../molecules/CompanySearchResultsItem.vue';
 import NoResults from '../atoms/NoResults.vue';
 import Button from '../atoms/Button.vue'
 
@@ -17,13 +17,13 @@ const firstThreeItems = (items: any[]) => items.slice(0, 3)
             }">
                 <ul class="grid grid-cols-3 gap-3">
                     <li v-for="company in firstThreeItems(items)" :key="company.id">
-                        <CompanySearchResultCard :company-name=company.name
+                        <CompanySearchResultsItem :company-name=company.name
                             :company-logo-url="`https://logo.clearbit.com/${company.url}`"
                             :contact-count="company.number_of_contacts" :href="`/companies/${company.id}`">
                             <template #name>
                                 <AisHighlight :hit="company" attribute="name" />
                             </template>
-                        </CompanySearchResultCard>
+                        </CompanySearchResultsItem>
                     </li>
                 </ul>
             </template>

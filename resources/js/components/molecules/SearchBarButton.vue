df91ce79abeaf4dc939b167942e0ffc3e2c55ba9
Strift <lau.cazanove@gmail.com>
2024-05-22 13:29:11 +0200

    Revert "Update search modal results UI"

--- resources/js/components/molecules/SearchBarButton.vue
+++ resources/js/components/molecules/SearchBarButton.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-const isMac = /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent)
+    const isMac = /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent)
 </script>
 
 <template>
     <button class="relative">
         <input class="h-10 text-sm placeholder-gray-300 border-gray-200 rounded-lg pe-10 focus:z-10"
-            placeholder="Search for anything" type="text" />
+        placeholder="Search" type="text" />
         <span v-if="isMac" class="absolute inset-y-0 p-2 text-xs text-gray-400 rounded-r-lg end-0 leading-5">
             ⌘
             K

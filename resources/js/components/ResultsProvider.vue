76cf5d08f11a20ff0ea7b784d04c9ab181e778cd
Strift <laurent@meilisearch.com>
2024-06-04 17:30:12 +0200

    lint

--- resources/js/components/ResultsProvider.vue
+++ resources/js/components/ResultsProvider.vue
@@ -6,10 +6,10 @@ import { AisStateResults } from 'vue-instantsearch/vue3/es'
   <ais-state-results>
     <template #default="{ results: { hits } }">
       <div v-if="hits.length">
-        <slot name="default"></slot>
+        <slot name="default" />
       </div>
       <div v-else>
-        <slot name="empty"></slot>
+        <slot name="empty" />
       </div>
     </template>
   </ais-state-results>

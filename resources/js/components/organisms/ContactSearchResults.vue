3ccce13cef79432e2a2f99d04bfc544a9340b881
Strift <laurent@meilisearch.com>
2024-06-04 17:29:04 +0200

    lint

--- resources/js/components/organisms/ContactSearchResults.vue
+++ resources/js/components/organisms/ContactSearchResults.vue
@@ -9,20 +9,34 @@ import NoResults from '../atoms/NoResults.vue'
 <template>
   <AisIndex index-name="contacts">
     <AisInfiniteHits>
-      <template #default="{
-        items,
-        refineNext,
-        isLastPage
-      }">
+      <template
+        #default="{
+          items,
+          refineNext,
+          isLastPage
+        }"
+      >
         <ul class="space-y-3">
-          <li v-for="contact, key in items" :key="key">
-            <ContactSearchResultsItem :href="`/contacts/${contact.id}`"
-              :contact-avatar-url="`https://api.dicebear.com/6.x/initials/svg?seed=${contact.name}`">
+          <li
+            v-for="contact, key in items"
+            :key="key"
+          >
+            <ContactSearchResultsItem
+              :href="`/contacts/${contact.id}`"
+              :contact-avatar-url="`https://api.dicebear.com/6.x/initials/svg?seed=${contact.name}`"
+            >
               <template #name>
-                <AisHighlight :hit="contact" attribute="name" />
+                <AisHighlight
+                  :hit="contact"
+                  attribute="name"
+                />
               </template>
               <template #email>
-                <AisHighlight :hit="contact" attribute="email" class="break-all" />
+                <AisHighlight
+                  :hit="contact"
+                  attribute="email"
+                  class="break-all"
+                />
               </template>
             </ContactSearchResultsItem>
           </li>

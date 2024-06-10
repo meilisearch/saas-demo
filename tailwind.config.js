0683d22638e3a90465f9d95912e5903f8d06ace2
Strift <laurent@meilisearch.com>
2024-06-10 12:01:47 +0200

    disable search input decorations

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    require('tailwindcss/plugin')(function ({addBase}) {
+      addBase({
+        '[type="search"]::-webkit-search-decoration': {display: 'none'},
+        '[type="search"]::-webkit-search-cancel-button': {display: 'none'},
+        '[type="search"]::-webkit-search-results-button': {display: 'none'},
+        '[type="search"]::-webkit-search-results-decoration': {display: 'none'},
+      })
+    }),
   ],
 }

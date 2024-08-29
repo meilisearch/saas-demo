a19be354a83a3c7ed79bc708b641cde1ed7f5bf4
Strift <laurent@meilisearch.com>
2024-08-29 17:21:41 +0200

    remove unused components

--- resources/views/components/atoms/input.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-  'placeholder' => ''
-])
-
-
-<input {{ $attributes->merge(['class' => 'h-10 text-sm placeholder-gray-300 border-gray-200 rounded-lg pe-10 focus:z-10']) }}
-  placeholder="{{ $placeholder }}"
-  type="text"
-/>
